56f6bb653dec326486af178a03c51f5d50241b7f
clerdson Juca Barbosa <clerdson@gmail.com>
2020-02-25 20:38:12 -0300

    init repositorio

---
+++ app/src/main/java/com/example/scrollingshooter/GameActivity.java
@@ -0,0 +1,18 @@
+package com.example.scrollingshooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Display;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Ponint
+    }
+}
